092c887f5a2109fe1edf34a642cb8f294d0766b6
nfxpnk <nfxpnk@gmail.com>
2019-02-19 14:31:16 +0200

    new user script

---
+++ jira.user.custom.js
@@ -0,0 +1,52 @@
+// ==UserScript==
+// @name		Copy JIRA issue title with issue key
+// @namespace   https://jira.ontrq.com/browse/
+// @include	 https://jira.ontrq.com/browse/*
+// @version	 4
+// @grant	   none
+// ==/UserScript==
+
+var nfxpnk = {
+    ge: function(elementId) {
+        return document.getElementById(elementId);
+    },
+
+    appendInput: function(parentElement, value, style) {
+        var input = document.createElement('input');
+        input.type = 'text';
+        input.value = value;
+        if (typeof style != 'undefined') {
+            input.style.width = '80%';
+        }
+        input.onclick = function() {
+            input.select();
+        };
+        parentElement.parentNode.appendChild(input);
+    },
+
+    main: function() {
+        var issueKey = this.ge('key-val');
+        var text = this.ge('summary-val');
+        var issueType = this.ge('type-val').innerText;
+
+        var messageType = issueType.match(/Bug/) ? 'FIX' : 'DEV';
+
+        var messageTypeBranch = issueType.match(/Bug/) ? 'bugfix' : 'feature';
+
+        var fullText = issueKey.textContent + ':' + messageType + ': ' + text.textContent;
+
+        this.appendInput(issueKey, issueKey.textContent);
+        this.appendInput(text, fullText, true);
+
+        var summary = text.textContent.toLowerCase().replace(/[^ \w]+/g, '');
+        summary = summary.replace(/ +/g, '-');
+        //summary = summary.substring(0, 40);
+
+        var branchName = messageTypeBranch.toLowerCase() + '/' + issueKey.textContent + '-' + summary;
+        var gitCheckout = 'git checkout -b ' + messageTypeBranch.toLowerCase() + '/' + issueKey.textContent + '-fe';
+        this.appendInput(text, branchName, true);
+        this.appendInput(text, gitCheckout, true);
+    }
+};
+
+nfxpnk.main();
